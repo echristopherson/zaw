d8fb019fea2069726a5baa853918ba6bf2fb757b
Eric Christopherson <echristopherson@gmail.com>
2015-02-25 12:24:55 -0600

    work nicely with spaces in filenames
    TODO: handle filenames with LF (^J) in them. But NOTE: other zaw sources do
    not correctly handle LF in filenames either.

--- sources/find-files.zsh
+++ sources/find-files.zsh
@@ -1,7 +1,8 @@
 # zaw source for find files
 
 function zaw-src-find-file() {
-    candidates=(`find . -type f`)
+    #candidates=(`find . -type f`)
+    candidates=("${(@f)$(find . -type f)}")
     for candidate in ${candidates}
     do
         as_short=`echo ${candidate} | awk -F'/' '{if (NF>4){LASTDIR=NF-1; print $1"/"$2"/.../"$LASTDIR"/"$NF;} else {print $0}}'`
